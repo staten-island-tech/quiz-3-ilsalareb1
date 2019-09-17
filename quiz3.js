c48efe0c4a21ca4290e8e8717ac4a9ca7f1a88df
ilsalareb1 <50841067+ilsalareb1@users.noreply.github.com>
2019-09-16 22:32:40 -0400

    Add files via upload

--- quiz3.js
+++ quiz3.js
@@ -14,4 +14,11 @@ const bills = [140,45,270];
 
 const tips = [tipCalculator(bills[0]), tipCalculator(bills[1]), tipCalculator(bills[2])];
 
+const totalAmounts = [ bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2] ];
+
+var finalAmountPaid = [ totalAmounts[0], totalAmounts[1], totalAmounts[2]];
+const result = finalAmountPaid.filter (finalAmountPaid.length > 100 );
+
+console.log(result);
+
 
